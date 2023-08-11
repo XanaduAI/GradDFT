1583358f09bbde1994d7bc9007183040b7d237fe
Jack Baker <jsbaker1001@gmail.com>
2023-08-11 09:51:37 -0400

    license details and short decription in setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,21 @@ from setuptools import setup, find_packages
 with open('requirements.txt', 'r') as f:
     requirements = f.read().splitlines()
 
+short_description = \
+    """
+    A library for learning the density functional theory exchange and 
+    correlation functional from data.
+    """
+
 setup(
     name='grad_dft',
     version='0.1',
     author='Xanadu Quantum Technologies',
-    author_email='author@xanadu.ai',
-    description='',
+    author_email='jack.baker@xanadu.ai',
+    description=short_description,
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    # url='https://github.com/your_username/package_name',
+    url='https://github.com/XanaduAi/grad_dft',
     packages=find_packages(),
     install_requires=requirements,
     extras_require={
@@ -20,18 +26,12 @@ setup(
             "openfermion>=1.5.1", "tqdm>=4.66.1", "torch>=2.0.1",
             "matplotlib>=3.7.2", "pandas>=2.0.3", "seaborn>=0.12.2"
         ],
-    },
-    # classifiers=[
-    #     'Development Status :: 3 - Alpha',
-    #     'Intended Audience :: Developers',
-    #     'License :: OSI Approved :: MIT License',
-    #     'Programming Language :: Python :: 3',
-    #     'Programming Language :: Python :: 3.6',
-    #     'Programming Language :: Python :: 3.7',
-    #     'Programming Language :: Python :: 3.8',
-    # ],    
+    },  
     classifiers=[
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research"
+        "License :: OSI Approved ::  Apache 2.0",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
